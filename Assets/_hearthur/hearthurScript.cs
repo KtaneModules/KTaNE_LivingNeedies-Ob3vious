09abbdc51fb4061deba523c5f2e5172e52b9896c
Ob3vious <niels.leest04@gmail.com>
2023-09-03 15:08:11 +0200

    Fix model and hide timer properly now

--- Assets/_hearthur/hearthurScript.cs
+++ Assets/_hearthur/hearthurScript.cs
@@ -1,104 +1,107 @@
 ﻿// You wanted to explore him inside I see?
 
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 using Rnd = UnityEngine.Random;
 
 public class hearthurScript : MonoBehaviour
 {
 
-	public KMAudio Audio;
-	public KMNeedyModule Module;
-	public KMSelectable Module2;
-	public Transform Heart;
+    public KMAudio Audio;
+    public KMNeedyModule Module;
+    public KMSelectable Module2;
+    public Transform Heart;
+    public Transform[] Contents;
 
-	private bool positioned = false;
-	private bool holding = false;
-	private bool active = false;
+    private bool timerRemoved = false;
+    private bool holding = false;
+    private bool active = false;
 
-	void Awake()
-	{
-		Module.OnNeedyActivation += OnNeedyActivation;
-		Module2.OnFocus += delegate { holding = true; };
-		Module2.OnDefocus += delegate { holding = false; };
-	}
+    void Awake()
+    {
+        Module.OnNeedyActivation += OnNeedyActivation;
+        Module2.OnFocus += delegate { holding = true; };
+        Module2.OnDefocus += delegate { holding = false; };
+    }
 
-	// Just used to get rid of normal neediness
-	void Update()
-	{
-		if (!positioned)
-		{
-			Module.transform.localScale /= 2f;
-			positioned = true;
-			StartCoroutine(HeartBeat());
-		}
-		Module.SetNeedyTimeRemaining(999999f);
-	}
+    // Just used to get rid of normal neediness
+    void Update()
+    {
+        if (!timerRemoved)
+            foreach (Transform child in transform)
+                if (!Contents.Contains(child) && child.gameObject.name.Contains("NeedyTimer"))
+                {
+                    timerRemoved = true;
+                    child.localScale = Vector3.zero;
+                }
+        Module.SetNeedyTimeRemaining(999999f);
+    }
 
-	protected void OnNeedyActivation()
-	{
-		active = true;
-		StartCoroutine(PanicTime());
-	}
+    protected void OnNeedyActivation()
+    {
+        active = true;
+        StartCoroutine(PanicTime());
+    }
 
-	private IEnumerator PanicTime()
-	{
-		while (active)
-		{
-			if (holding)
-			{
-				holding = false;
-				active = false;
-				Module.HandleStrike();
-				Module.HandlePass();
-			}
-			yield return null;
-		}
-		yield return null;
-	}
+    private IEnumerator PanicTime()
+    {
+        while (active)
+        {
+            if (holding)
+            {
+                holding = false;
+                active = false;
+                Module.HandleStrike();
+                Module.HandlePass();
+            }
+            yield return null;
+        }
+        yield return null;
+    }
 
-	private IEnumerator HeartBeat()
-	{
-		while (true)
-		{
+    private IEnumerator HeartBeat()
+    {
+        while (true)
+        {
             if (active)
             {
-				Audio.PlaySoundAtTransform("HeartBeat1", Heart);
-				float x = Rnd.Range(0.1f, 0.25f);
-				for (float t = 0; t < 1; t += Time.deltaTime / x)
-				{
-					Heart.localScale = Vector3.Lerp(new Vector3(7f, 7f, 8f), new Vector3(8f, 8f, 8f), t);
-					yield return null;
-				}
-				Audio.PlaySoundAtTransform("HeartBeat2", Heart);
-				x = Rnd.Range(0.25f, 0.75f);
-				for (float t = 0; t < 1; t += Time.deltaTime / x)
-				{
-					Heart.localScale = Vector3.Lerp(new Vector3(7f, 7f, 8f), new Vector3(8f, 8f, 8f), t);
-					yield return null;
-				}
-			}
-			yield return null;
-		}
-	}
+                Audio.PlaySoundAtTransform("HeartBeat1", Heart);
+                float x = Rnd.Range(0.1f, 0.25f);
+                for (float t = 0; t < 1; t += Time.deltaTime / x)
+                {
+                    Heart.localScale = Vector3.Lerp(new Vector3(7f, 7f, 8f), new Vector3(8f, 8f, 8f), t);
+                    yield return null;
+                }
+                Audio.PlaySoundAtTransform("HeartBeat2", Heart);
+                x = Rnd.Range(0.25f, 0.75f);
+                for (float t = 0; t < 1; t += Time.deltaTime / x)
+                {
+                    Heart.localScale = Vector3.Lerp(new Vector3(7f, 7f, 8f), new Vector3(8f, 8f, 8f), t);
+                    yield return null;
+                }
+            }
+            yield return null;
+        }
+    }
 
-	#pragma warning disable 414
-	private string TwitchHelpMessage = "Please don't send anything while I'm beating. You'll only cause problems.";
-	#pragma warning restore 414
-	IEnumerator ProcessTwitchCommand(string command)
-	{
-		yield return null;
-		if (active)
-			yield return "strike";
-	}
+#pragma warning disable 414
+    private string TwitchHelpMessage = "Please don't send anything while I'm beating. You'll only cause problems.";
+#pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        yield return null;
+        if (active)
+            yield return "strike";
+    }
 
-	IEnumerator TwitchHandleForcedSolve()
-	{
-		while (true)
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        while (true)
         {
-			holding = false;
-			yield return true;
-		}
-	}
+            holding = false;
+            yield return true;
+        }
+    }
 
 }
--- Assets/_kawaiitlyn/kawaiitlynScript.cs
+++ Assets/_kawaiitlyn/kawaiitlynScript.cs
@@ -1,94 +1,99 @@
 ﻿// You wanted to explore her inside I see?
 
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Rnd = UnityEngine.Random;
 
 public class kawaiitlynScript : MonoBehaviour
 {
 
-	public KMAudio Audio;
-	public KMNeedyModule Module;
-	public KMSelectable Module2;
+    public KMAudio Audio;
+    public KMNeedyModule Module;
+    public KMSelectable Module2;
+    public Transform[] Contents;
 
-	private bool positioned = false;
-	private bool holding = false;
-	private int t = 0;
+    private bool timerRemoved = false;
+    private bool holding = false;
+    private int t = 0;
 
-	private static readonly int[][] Takes = { new int[] { 2, 2, 2, 2, 3, 2, 2 }, new int[] { 3, 3, 3, 3, 3, 3, 3, 3 }, new int[] { 3, 3, 3, 3, 3, 3, 3, 3 }, new int[] { 3, 3, 4, 4, 3, 3, 3 } };
+    private static readonly int[][] Takes = { new int[] { 2, 2, 2, 2, 3, 2, 2 }, new int[] { 3, 3, 3, 3, 3, 3, 3, 3 }, new int[] { 3, 3, 3, 3, 3, 3, 3, 3 }, new int[] { 3, 3, 4, 4, 3, 3, 3 } };
 
-	void Awake()
-	{
-		Module.OnNeedyActivation += OnNeedyActivation;
-		Module2.OnFocus += delegate { holding = true; };
-		Module2.OnDefocus += delegate { holding = false; };
-	}
-
-	// Just used to get rid of normal neediness
-	void Update()
-	{
-		if (!positioned)
-		{
-			Module.transform.localScale /= 2f;
-			positioned = true;
-		}
-		Module.SetNeedyTimeRemaining(999999f);
-	}
-
-	protected void OnNeedyActivation()
-	{
-		StartCoroutine(NoticeMe());
-	}
-
-	private IEnumerator NoticeMe()
-	{
-		//audio here maybe?
-		while (true)
-		{
-			if (holding)
-			{
-				if (t > 1)
-					t -= 2;
-				else if (t > 0)
-					t--;
-			}
-			else
-				t++;
-			if (t % 100 == 0 && t != 0 && !holding && t < 300)
-				PlayRandom(0);
-			else if (t % 50 == 0 && !holding && t >= 300 && t < 450)
-				PlayRandom(1);
-			else if (t % 30 == 0 && !holding && t >= 450 && t < 600)
-				PlayRandom(2);
-			else if (t % 20 == 0 && !holding && t >= 600)
-				PlayRandom(3);
-			yield return new WaitForSeconds(0.1f);
-		}
-	}
-
-	private void PlayRandom(int state)
+    void Awake()
     {
-		int pick = Rnd.Range(0, Takes[state].Length);
-		Audio.PlaySoundAtTransform(new string[] { "Neutral","Concern","Anxious","Desperate" }[state] + " line " + (pick + 1) + " take " + (Rnd.Range(0, Takes[state][pick]) + 1), Module.transform);
+        Module.OnNeedyActivation += OnNeedyActivation;
+        Module2.OnFocus += delegate { holding = true; };
+        Module2.OnDefocus += delegate { holding = false; };
     }
 
-	#pragma warning disable 414
-	private string TwitchHelpMessage = "Just send me any command to be with me.";
-	#pragma warning restore 414
-	IEnumerator ProcessTwitchCommand(string command)
-	{
-		yield return null;
-		while (t > 0)
-			yield return "trycancel";
-	}
+    // Just used to get rid of normal neediness
+    void Update()
+    {
+        if (!timerRemoved)
+            foreach (Transform child in transform)
+                if (!Contents.Contains(child) && child.gameObject.name.Contains("NeedyTimer"))
+                {
+                    timerRemoved = true;
+                    child.localScale = Vector3.zero;
+                }
+        Module.SetNeedyTimeRemaining(999999f);
+    }
 
-	IEnumerator TwitchHandleForcedSolve()
-	{
-		while (true)
-		{
-			holding = true;
-			yield return true;
-		}
-	}
+    protected void OnNeedyActivation()
+    {
+        StartCoroutine(NoticeMe());
+    }
+
+    private IEnumerator NoticeMe()
+    {
+        //audio here maybe?
+        while (true)
+        {
+            if (holding)
+            {
+                if (t > 1)
+                    t -= 2;
+                else if (t > 0)
+                    t--;
+            }
+            else
+                t++;
+            if (t % 100 == 0 && t != 0 && !holding && t < 300)
+                PlayRandom(0);
+            else if (t % 50 == 0 && !holding && t >= 300 && t < 450)
+                PlayRandom(1);
+            else if (t % 30 == 0 && !holding && t >= 450 && t < 600)
+                PlayRandom(2);
+            else if (t % 20 == 0 && !holding && t >= 600)
+                PlayRandom(3);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void PlayRandom(int state)
+    {
+        int pick = Rnd.Range(0, Takes[state].Length);
+        Audio.PlaySoundAtTransform(new string[] { "Neutral", "Concern", "Anxious", "Desperate" }[state] + " line " + (pick + 1) + " take " + (Rnd.Range(0, Takes[state][pick]) + 1), Module.transform);
+    }
+
+#pragma warning disable 414
+    private string TwitchHelpMessage = "Just send me any command to be with me.";
+#pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        yield return null;
+        while (t > 0)
+            yield return "trycancel";
+    }
+
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        while (true)
+        {
+            holding = true;
+            yield return true;
+        }
+    }
 
 }
